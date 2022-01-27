f945d1bb836ae85cc045c35bb3ebcf2d85519e77
Jérémy <jbarland@stagiaire-humanbooster.com>
2022-01-27 16:32:46 +0100

    start create fixtures

--- migrations/Version20220127150502.php
+++ migrations/Version20220127150502.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220103133549 extends AbstractMigration
+final class Version20220127150502 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -23,7 +23,7 @@ final class Version20220103133549 extends AbstractMigration
         $this->addSql('CREATE TABLE address (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, name VARCHAR(50) DEFAULT NULL, address1 VARCHAR(150) NOT NULL, address2 VARCHAR(150) DEFAULT NULL, postal_code VARCHAR(5) NOT NULL, town VARCHAR(255) NOT NULL, INDEX IDX_D4E6F81A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE address_type (purchase_id INT NOT NULL, address_id INT NOT NULL, type VARCHAR(20) NOT NULL, INDEX IDX_F19287C2558FBEB9 (purchase_id), INDEX IDX_F19287C2F5B7AF75 (address_id), PRIMARY KEY(purchase_id, address_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE brand (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, picture VARCHAR(255) DEFAULT NULL, country VARCHAR(50) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, id_parent INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, parent_id INT DEFAULT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, INDEX IDX_64C19C1727ACA70 (parent_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, created_at DATETIME NOT NULL, price DOUBLE PRECISION NOT NULL, weight DOUBLE PRECISION DEFAULT NULL, end_at DATETIME DEFAULT NULL, is_validate TINYINT(1) DEFAULT NULL, status VARCHAR(50) NOT NULL, INDEX IDX_F5299398A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, product_id INT DEFAULT NULL, INDEX IDX_16DB4F894584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, category_id INT DEFAULT NULL, brand_id INT DEFAULT NULL, ean VARCHAR(13) NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, created_at DATETIME NOT NULL, picture VARCHAR(255) DEFAULT NULL, stock INT DEFAULT NULL, price DOUBLE PRECISION DEFAULT NULL, is_active TINYINT(1) NOT NULL, INDEX IDX_D34A04AD12469DE2 (category_id), INDEX IDX_D34A04AD44F5D008 (brand_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -31,6 +31,7 @@ final class Version20220103133549 extends AbstractMigration
         $this->addSql('ALTER TABLE address ADD CONSTRAINT FK_D4E6F81A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE address_type ADD CONSTRAINT FK_F19287C2558FBEB9 FOREIGN KEY (purchase_id) REFERENCES `order` (id)');
         $this->addSql('ALTER TABLE address_type ADD CONSTRAINT FK_F19287C2F5B7AF75 FOREIGN KEY (address_id) REFERENCES address (id)');
+        $this->addSql('ALTER TABLE category ADD CONSTRAINT FK_64C19C1727ACA70 FOREIGN KEY (parent_id) REFERENCES category (id)');
         $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE picture ADD CONSTRAINT FK_16DB4F894584665A FOREIGN KEY (product_id) REFERENCES product (id)');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
@@ -42,6 +43,7 @@ final class Version20220103133549 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE address_type DROP FOREIGN KEY FK_F19287C2F5B7AF75');
         $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04AD44F5D008');
+        $this->addSql('ALTER TABLE category DROP FOREIGN KEY FK_64C19C1727ACA70');
         $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04AD12469DE2');
         $this->addSql('ALTER TABLE address_type DROP FOREIGN KEY FK_F19287C2558FBEB9');
         $this->addSql('ALTER TABLE picture DROP FOREIGN KEY FK_16DB4F894584665A');
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -7,6 +7,7 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use JetBrains\PhpStorm\Pure;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 #[ApiResource(
@@ -17,23 +18,27 @@ class Category
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
-    private $id;
+    private int $id;
 
     #[ORM\Column(type: 'string', length: 255)]
-    private $title;
+    private string $title;
 
     #[ORM\Column(type: 'text', nullable: true)]
-    private $description;
+    private string $description;
 
     #[ORM\OneToMany(mappedBy: 'category', targetEntity: Product::class)]
     private $products;
 
-    #[ORM\Column(type: 'integer')]
-    private $idParent;
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'id_parent')]
+    private ?Category $parent;
 
-    public function __construct()
+    #[ORM\OneToMany(mappedBy: 'parent', targetEntity: self::class)]
+    private Collection $id_parent;
+
+    #[Pure] public function __construct()
     {
         $this->products = new ArrayCollection();
+        $this->id_parent = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -95,14 +100,43 @@ class Category
         return $this;
     }
 
-    public function getIdParent(): ?int
+    public function getParent(): ?self
     {
-        return $this->idParent;
+        return $this->parent;
     }
 
-    public function setIdParent(int $idParent): self
+    public function setParent(?self $parent): self
     {
-        $this->idParent = $idParent;
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|self[]
+     */
+    public function getIdParent(): Collection
+    {
+        return $this->id_parent;
+    }
+
+    public function addIdParent(self $idParent): self
+    {
+        if (!$this->id_parent->contains($idParent)) {
+            $this->id_parent[] = $idParent;
+            $idParent->setParent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeIdParent(self $idParent): self
+    {
+        if ($this->id_parent->removeElement($idParent)) {
+            if ($idParent->getParent() === $this) {
+                $idParent->setParent(null);
+            }
+        }
 
         return $this;
     }
