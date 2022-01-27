4bebf41bcb5740908ced25c8300b83d068dd6b5a
Jérémy <jbarland@stagiaire-humanbooster.com>
2022-01-27 15:20:43 +0100

    add fontawesome and alicebundle

--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,9 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    .addEntry('styles', './assets/styles/main.scss')
+    .addEntry('scripts', './assets/scripts/main.ts')
 
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
